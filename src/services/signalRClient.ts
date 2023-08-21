43010c2d17e36ec1fcec64c9d23ced2229d76189
Andre <andre.augusto22@hotmail.com>
2023-08-20 21:38:37 -0300

    tabuleiro

---
+++ src/services/signalRClient.ts
@@ -0,0 +1,10 @@
+import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
+
+const createHubConnection = (hubUrl: string) => {
+  return new HubConnectionBuilder()
+    .withUrl(hubUrl)
+    .withAutomaticReconnect()
+    .build();
+};
+
+export default createHubConnection;
