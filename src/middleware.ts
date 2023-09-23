6dcfa1d0d2b55c17b2e546311f1c2007e45563ef
LuizFVC <lvfeblue3@gmail.com>
2023-09-23 16:36:58 -0300

    login

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export default function middleware(request: NextRequest) {
+  const token = request.cookies.get("auth_token")?.value;
+
+  const signInURL = new URL("/login", request.url);
+
+  if (!token)
+    if (request.nextUrl.pathname === "/login") {
+      return NextResponse.next();
+    }
+  return NextResponse.redirect(signInURL);
+}
+
+export const config = {
+  matcher: "/:path*",
+};
