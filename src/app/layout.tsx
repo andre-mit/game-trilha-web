6413313fe50b67b53446856e9d1b037f7bffdcd7
Andre <andre.augusto22@hotmail.com>
2023-11-02 17:05:35 -0300

    AB#18 Remove user context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { UserProvider } from "@/context/userContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={`${inter.className} min-h-screen`}>
-        <UserProvider>{children}</UserProvider>
+        {children}
       </body>
     </html>
   );
